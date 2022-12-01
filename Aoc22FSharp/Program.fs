06b623331c300f74ea47279e8aa7c796964fe69c
Dawid Wysocki <dwysocki157@gmail.com>
2022-12-01 08:51:09 +0100

    Add project files.

---
+++ Aoc22FSharp/Program.fs
@@ -0,0 +1,12 @@
+﻿printfn "Hello from F#"
+
+let getElvesInventoryFromFile path =
+    let lines = System.IO.File.ReadAllLines path
+    let emptyLineIndexes = lines |> Array.indexed |> Array.filter (fun (i, line) -> line = "") |> Array.map fst
+    let splitByEmptyLines = emptyLineIndexes[.. emptyLineIndexes.Length - 2] |> Array.indexed |> Array.map (fun (i, _) -> lines.[i..(emptyLineIndexes.[i + 1] - 1)])
+    // map result to int
+    splitByEmptyLines |> Array.map (fun elf -> elf |> Array.map (fun s -> int s))
+ 
+let findResult1 (elves : int[][]) =
+    elves |> Array.map (fun elf -> elf |> Array.sum) |> Array.max
+
